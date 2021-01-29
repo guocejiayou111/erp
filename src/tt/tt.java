96f758c46661c9a2f57f6680d2600c68041cdd56
guoce <guoce888@163.com>
2021-01-29 17:26:43 +0800

    add hello.java

--- src/tt/tt.java
+++ src/tt/tt.java
@@ -6,6 +6,6 @@ public class tt {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Date date = new Date();
-		System.out.println("@@@"+date);
+		System.out.println("@@@111"+date);
 	}
 }
