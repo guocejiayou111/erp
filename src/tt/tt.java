99942693b079e3876ec5651e1708dfb753813394
guoce <guoce888@163.com>
2021-01-29 17:09:31 +0800

    create a project

---
+++ src/tt/tt.java
@@ -0,0 +1,11 @@
+package tt;
+
+import java.util.Date;
+
+public class tt {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date date = new Date();
+		System.out.println("@@@"+date);
+	}
+}
